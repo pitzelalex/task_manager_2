987c2bee74b4980948a7a1ade1ef7cea80832702
Alex Pitzel <C-Alexander.Pitzel@charter.com>
2025-01-31 12:55:35 -0500

    Add basic test

--- spec/requests/api/v1/tasks_request_spec.rb
+++ spec/requests/api/v1/tasks_request_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks api" do
-  before(:all) do
+
+  it "sends a list of Tasks" do
     Task.create(title: "Feed the cat", description: "Test")
     Task.create(title: "Do the dishes", description: "Test")
-  
-  end
-  
-  it "sends a list of Tasks" do
     get "/api/v1/tasks"
 
+    tasks = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
+    expect(tasks.count).to be(2)
   end
 end
